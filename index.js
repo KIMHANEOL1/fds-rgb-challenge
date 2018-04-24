287e11c80b5f8b99507364b8c9abb1cc03aca691
Kim HanEol <himnemesis@gmail.com>
2018-04-24 16:41:03 +0900

    fds-rgb-challenge

---
+++ index.js
@@ -0,0 +1,54 @@
+//문자열이 랜덤으로 나오게 하는 코드
+function randomNumber(){
+    return Math.floor(256*math.random());   //256보다 작은 수가 랜덤으로 나오게 함
+}
+
+function randomColorCode(){
+    return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+}
+
+
+const colorCodeEl = document.querySelector('.color-code');
+const boxes = document.querySelectorAll('.box');
+
+let correctAnswer;
+let score=0;
+document.querySelector('.score').textContent = score;
+
+function newStage(){
+    const colorCodes=[randomColorCode(),randomColorCode(),randomColorCode()]
+    boxes.forEach((el,index)=>{
+        el.style.backgroundColor = colorCodes[index];
+    });
+    correctAnswer = Math.floor(Math.random() * 3);
+    colorCodeEl.textContent= colorCodes[correctAnswer];
+}
+
+
+boxes.forEach((el, index)=>{
+    el.addEventListener('click',()=>{
+        el.classList.add('large');
+        if(correctAnswer === index){
+            
+            document.querySelector('.modal.right').classList.add('show');
+            score++;
+        }else{
+            
+            score=0;
+        }
+        document.querySelector('.score').textContent = score;
+    })
+    
+})
+
+document.querySelector('.modal.right.close').addEventListener('click',()=>{
+    newStage();
+    boxes.forEach(el =>{
+    el.classList.remove('show');
+});
+    document.querySelector('.modal.right').classList.remove('show');
+})
+
+
+newStage();
+
